9a35aa3cf0a84b50cdbca57877e1ad5b203ef688
coolshaddy <coolshaddy81@gmail.com>
2021-02-01 17:57:02 +0530

    first commit for travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           demo website
+           demo website for demo repository
         </p>
         <a
           className="App-link"
